b8cd614d3a916f93d469cef05cf1233cb79e1a8b
oklincov <ognjen.klincov@planetsoft.eu>
2025-09-27 18:10:01 +0200

    Refactoring

--- budget-ok-frontend-web/src/App.tsx
+++ budget-ok-frontend-web/src/App.tsx
@@ -1,24 +1,19 @@
-import {App as AntdApp} from 'antd';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { App as AntdApp } from 'antd';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
-import CreateEnvelopeForm from "./components/CreateEnvelopeForm/CreateEnvelopeForm.tsx";
 import EnvelopesTable from "./components/EnvelopesTable/EnvelopesTable";
 
 const queryClient = new QueryClient();
 
 function AppContent() {
   return (
-      <div className="app-container">
-        <h1>Budget OK</h1>
-        <div className="card" style={{marginTop: '24px'}}>
-          <h2>Your Envelopes</h2>
-          <EnvelopesTable/>
-        </div>
-        <div className="card">
-          <h2>Create New Envelope</h2>
-          <CreateEnvelopeForm/>
-        </div>
+    <div className="app-container">
+      <h1>Budget OK</h1>
+      <div className="card" style={{ marginTop: '24px' }}>
+        <h2>Your Envelopes</h2>
+        <EnvelopesTable />
       </div>
+    </div>
   );
 }
 
